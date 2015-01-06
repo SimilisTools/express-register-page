cce871b984f12fddbc8081941b92adc483219488
Toni Hermoso Pulido <toniher@cau.cat>
2015-01-06 19:19:47 +0100

    adding stuff

---
+++ index.js
@@ -0,0 +1,44 @@
+var express=require('express');
+var nconfig = require('./config.js');
+var config = nconfig.get("express");
+var errorhandler = require("errorhandler");
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var nodemailer = require("nodemailer");
+
+var lessMiddleware = require('less-middleware');
+
+var basepath = "";
+
+if (config.basepath) {
+	basepath = config.basepath;
+}
+
+var app=express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Config
+app.use(errorhandler({ dumpExceptions: true, showStack: true }));
+
+// Compression
+app.use(compression({
+  threshold: 512
+}))
+
+app.set("config", config);
+
+// Launch server
+var server = app.listen(config.port);
+
+
+// Now views
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
+app.use(lessMiddleware(__dirname + '/public')); // TODO: Minor, allow other paths
+app.use(basepath, express.static(__dirname + '/public'))
+
+
+console.log("Seqserver listening on port " + config.port);
