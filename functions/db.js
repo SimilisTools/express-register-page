87d6585a58b4218275e10f3f52b37ddf2e4831d6
Toni Hermoso Pulido <toniher@cau.cat>
2015-01-09 19:35:32 +0100

    DB

--- functions/db.js
+++ functions/db.js
@@ -14,21 +14,51 @@ exports.addinDB = function( params, cb ){
 		db.serialize(function() {
 			db.run('create table if not exists Register (name TEXT, email TEXT, strid TEXT, verified BOOLEAN)');
 		
-			var stmt = db.prepare("INSERT INTO Register (name, email, strid, verified) VALUES ('"+ params.name + "', '" + params.email + "', 'XXXXX', 0 )");
-			stmt.run();
-			stmt.finalize();
-			info = { "Saved": "OK" };
+			// Let's check if email
+			db.all("SELECT verified from Register where email = '" + params.email + "'", function(err, rows) {
+
+				if ( !err ) {
+
+					if ( rows.length < 1 ) {
+						var stmt = db.prepare("INSERT INTO Register (name, email, strid, verified) VALUES ('"+ params.name + "', '" + params.email + "', 'XXXXX', 0 )");
+						stmt.run();
+						stmt.finalize();
+						info = { "msg": "Saved" };
+						cb( err, info );
+						db.close();
+					} else {
+						if ( rows[0] ) {
+							if ( rows[0].verified ) {
+								info = { "msg": "Validated" };
+								cb( err, info );
+								db.close();
+							} else {
+								var stmt = db.prepare("UPDATE Register SET name = '"+ params.name + "', strid = 'UUUU' where email='"+params.email+"'");
+								stmt.run();
+								stmt.finalize();
+								info = { "msg": "Updated" };
+								cb( err, info );
+								db.close();
+							}
+						}
+					}
+				} else {
+						err = {
+							"msg": "DB Query Error "
+						}
+					cb( err, info );
+					db.close()
+				}
+			});
 		});
 		
-		db.close();
 	} else {
 		err = {
 			"msg": "No DB connection"
 		}
+		cb( err, info );
 	}
-	
-	cb( err, info );
-	
+
 };
 
 exports.listinDB = function( params, cb ){
@@ -40,7 +70,7 @@ exports.listinDB = function( params, cb ){
 
 		var db = new sqlite3.Database( dbfile );
 		db.serialize(function() {
-			var out = [];
+
 			db.all("SELECT name from Register", function(err, rows) {
 				if (! err ) {
 					cb( err, rows );
