515053102d1ef2330c379be42f251466bfe8b3fe
Toni Hermoso Pulido <toniher@cau.cat>
2015-01-16 19:04:00 +0100

    more coordis

--- public/main.js
+++ public/main.js
@@ -41,5 +41,23 @@ $( document ).ready(function() {
 			}
 		});
 	})();
+	
+	(function mapinit() {
+		var map = new ol.Map({
+		target: 'map',
+		layers: [
+		  new ol.layer.Tile({
+			source: new ol.source.MapQuest({layer: 'sat'})
+		  })
+		],
+		view: new ol.View({
+		  center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
+		  zoom: 8
+		})
+		});
+	})();
+	
 
 });
+
+
