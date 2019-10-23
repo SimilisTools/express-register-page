04f8b9190709c6055039f3fc1be807b0d385633f
Toni Hermoso Pulido <toniher@cau.cat>
2019-10-23 23:49:23 +0200

    Update method

--- functions/sm.js
+++ functions/sm.js
@@ -2,54 +2,41 @@
 
 var nodemailer = require('nodemailer');
 var sendmailTransport = require('nodemailer-sendmail-transport');
-var emailTemplates = require('email-templates');
+var ejs = require("ejs");
 
 exports.sendMail = function( params, method, extra, cb ) {
 
-	var transporter;
-	if ( params.type === 'sendmail' ) {
-		var options = {
-			"path": params.path
-		};
-		transporter = nodemailer.createTransport( sendmailTransport( options ) ) ;
-	} else {
-		transporter = nodemailer.createTransport({
-			type: params.type
-		});
-	}
-	
-	var subject = "["+extra.event.title+"] - "+method;
-	
-	console.log( subject );
+	ejs.renderFile( params.templates + "/" + method + ".ejs", params, function (err, data) {
 
-	transporter.sendMail(  {
-			from: params.from,
-			to: extra.to,
-			subject: subject,
-			//html: html,
-			//text: text
-			text: "Test msg"
-	}, function(err, info){
-		console.log( info );
-		console.log( err );
-		cb( err, info );
+		var transporter;
+		if ( params.type === 'sendmail' ) {
+			var options = {
+				"path": params.path
+			};
+			transporter = nodemailer.createTransport( sendmailTransport( options ) ) ;
+		} else {
+			transporter = nodemailer.createTransport({
+				type: params.type
+			});
+		}
+		
+		var subject = "["+extra.event.title+"] - "+method;
+		
+		console.log( subject );
+	
+		transporter.sendMail(  {
+				from: params.from,
+				to: extra.to,
+				subject: subject,
+				html: data,
+				//text: text
+				text: "Test msg"
+		}, function(err, info){
+			console.log( info );
+			console.log( err );
+			cb( err, info );
+		});
+	
 	});
 
-	//new emailTemplates( params.templates, function(err, template ) {
-	//
-	//	if (err) {
-	//		console.log(err);
-	//	} else {
-	//
-	//		// Send a single email
-	//		template( method, extra, function(err, html, text) {
-	//			if (err) {
-	//					console.log(err);
-	//			} else {
-	//			// send mail with defined transport object
-	//				
-	//			}
-	//		});
-	//	}
-	//});
 };
