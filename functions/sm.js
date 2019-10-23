262c43fdfa19cdfc9fc96ab08d260f1db9b09d50
Toni Hermoso Pulido <toniher@cau.cat>
2019-10-23 22:56:19 +0200

    simplify

--- functions/sm.js
+++ functions/sm.js
@@ -6,46 +6,50 @@ var emailTemplates = require('email-templates');
 
 exports.sendMail = function( params, method, extra, cb ) {
 
-	new emailTemplates( params.templates, function(err, template ) {
+	var transporter;
+	if ( params.type === 'sendmail' ) {
+		var options = {
+			"path": params.path
+		};
+		transporter = nodemailer.createTransport( sendmailTransport( options ) ) ;
+	} else {
+		transporter = nodemailer.createTransport({
+			type: params.type
+		});
+	}
+	
+	var subject = "["+extra.event.title+"] - "+method;
+	
+	console.log( subject );
 
-		if (err) {
-			console.log(err);
-		} else {
-		
-			var transporter;
-			if ( params.type === 'sendmail' ) {
-				var options = {
-					"path": params.path
-				};
-				transporter = nodemailer.createTransport( sendmailTransport( options ) ) ;
-			} else {
-				transporter = nodemailer.createTransport({
-					type: params.type
-				});
-			}
-			
-			var subject = "["+extra.event.title+"] - "+method;
-			
-			console.log( subject );
-			// Send a single email
-			template( method, extra, function(err, html, text) {
-				if (err) {
-						console.log(err);
-				} else {
-				// send mail with defined transport object
-					transporter.sendMail(  {
-						from: params.from,
-						to: extra.to,
-						subject: subject,
-						html: html,
-						text: text
-					}, function(err, info){
-						console.log( info );
-						console.log( err );
-						cb( err, info );
-					});
-				}
-			});
-		}
+	transporter.sendMail(  {
+			from: params.from,
+			to: extra.to,
+			subject: subject,
+			//html: html,
+			//text: text
+			text: "Test msg"
+	}, function(err, info){
+		console.log( info );
+		console.log( err );
+		cb( err, info );
 	});
+
+	//new emailTemplates( params.templates, function(err, template ) {
+	//
+	//	if (err) {
+	//		console.log(err);
+	//	} else {
+	//
+	//		// Send a single email
+	//		template( method, extra, function(err, html, text) {
+	//			if (err) {
+	//					console.log(err);
+	//			} else {
+	//			// send mail with defined transport object
+	//				
+	//			}
+	//		});
+	//	}
+	//});
 };
